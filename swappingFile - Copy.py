338044b40a6ddb9de7dfbef66fc6509991c4daa6
WhizzCoder13 <83590696+WhizzCoder13@users.noreply.github.com>
2022-04-18 13:04:42 +0100

    Add files via upload

---
+++ swappingFile - Copy.py
@@ -0,0 +1,11 @@
+fed swapFileDate()
+    fileName =  input("Enter the file name:- ")
+
+    data_a = sampletext1
+    data_b = sampletext2
+
+    file =  open(fileName, 'r')
+        sampletext1 = sampletext2
+        data_a = data_b
+
+swapFileDate()
